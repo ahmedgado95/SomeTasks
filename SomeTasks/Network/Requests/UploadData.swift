d1514d3924116ddadd659c52d0c13dbfdbd153a7
ahmed gado <ahmedgado@Ahmed.local>
2020-12-20 18:46:24 +0200

    first commit

---
+++ SomeTasks/Network/Requests/UploadData.swift
@@ -0,0 +1,9 @@
+//
+//  UploadData.swift
+//  SomeTasks
+//
+//  Created by ahmed gado on 20/12/2020.
+//  Copyright © 2020 ahmedgado. All rights reserved.
+//
+
+import Foundation
---
+++ SomeTasks/Scenes/FibonacciVC.swift
@@ -0,0 +1,30 @@
+//
+//  FibonacciVC.swift
+//  SomeTasks
+//
+//  Created by ahmed gado on 20/12/2020.
+//  Copyright © 2020 ahmedgado. All rights reserved.
+//
+
+import UIKit
+
+class FibonacciVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
